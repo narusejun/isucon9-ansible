350b18e70c8bdb4be69098a7f4149ef537cf7117
Kazuki Sawada <kazuki@6715.jp>
2019-09-04 21:41:24 +0900

    mv phpmyadmin from local to hq

---
+++ roles/hq.phpmyadmin/templates/config.inc.php
@@ -0,0 +1,13 @@
+
+{% for item in groups['all'] %}
+{% if 'mysql_forward' in hostvars[item] %}
+<?php
+	$i++;
+	$cfg["Servers"][$i]["host"] = "127.0.0.1";
+	$cfg["Servers"][$i]["port"] = "{{ hostvars[item]['mysql_forward'] }}";
+	$cfg["Servers"][$i]["auth_type"] = "config";
+	$cfg["Servers"][$i]["user"] = "{{ bot_user }}";
+	$cfg["Servers"][$i]["password"] = "{{ bot_user }}";
+?>
+{% endif %}
+{% endfor %}
