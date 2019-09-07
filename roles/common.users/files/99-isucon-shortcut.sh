5bfe2afc7bfd2f600650ac524dc16af3ea917351
Kazuki Sawada <kazuki@6715.jp>
2019-09-07 19:16:40 +0900

    update shortcuts

--- roles/common.users/files/99-isucon-shortcut.sh
+++ roles/common.users/files/99-isucon-shortcut.sh
@@ -1,12 +1,19 @@
 alias systemctl="sudo systemctl"
 alias jounalctl="sudo journalctl"
+
 alias sc="sudo systemctl"
 alias sce="sudo systemctl status"
 alias scs="sudo systemctl start"
 alias sck="sudo systemctl stop"
 alias scr="sudo systemctl restart"
 alias scl="sudo systemctl reload"
+alias scon="sudo systemctl enable"
+alias scof="sudo systemctl disable"
+
 alias jc="sudo journalctl"
 alias jcf="sudo journalctl -f -u"
 alias jcn="sudo journalctl -n 100 -u"
 alias jcnn="sudo journalctl -n 1000 -u"
+
+alias ppf="sudo /opt/go/bin/go tool pprof -http :8080"
+alias pph="sudo /opt/go/bin/go tool pprof -http :8080 http://localhost:6060/debug/pprof/profile"
