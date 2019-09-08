ab277edba5f91084d8c41fb8c62fb6bb39d37598
Kazuki Sawada <kazuki@6715.jp>
2019-09-08 18:00:31 +0900

    3台構成

--- roles/app.core/templates/env.sh
+++ roles/app.core/templates/env.sh
@@ -1,13 +1,12 @@
-MYSQL_HOST=127.0.0.1
 MYSQL_PORT=3306
 MYSQL_USER=isucari
 MYSQL_DBNAME=isucari
 MYSQL_PASS=isucari
 
 {% if inventory_hostname == "isu1.sysad.net" %}
-
+MYSQL_HOST=isu3.sysad.net
 {% elif inventory_hostname == "isu2.sysad.net" %}
-
+MYSQL_HOST=isu3.sysad.net
 {% elif inventory_hostname == "isu3.sysad.net" %}
-
+MYSQL_HOST=127.0.0.1
 {% endif %}
